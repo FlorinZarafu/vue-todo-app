64ac22948859623dbff7df5719d0e50346e22f81
Florin Zarafu <zrflorin@gmail.com>
2020-06-12 12:15:41 +0300

    commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo" />
+      <input type="submit" class="btn" value="Add Todo" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        title: this.title,
+        completed: false,
+      };
+      //   send to parent
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>Todo List</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped></style>
